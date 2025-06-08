54d3a0c470ba82b04769a6e0bc97c661de298328
patluka <lukaszewski.patryk06@gmail.com>
2025-06-08 11:29:05 +0200

    Add redirect when hash not found

--- redirect/__init__.py
+++ redirect/__init__.py
@@ -4,17 +4,19 @@ import requests
 import os
 
 SERVICE_DB_URL = os.getenv("SERVICE_DB_URL")
+SERVICE_GUI_URL = os.getenv("SERVICE_GUI_URL")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     h = req.route_params.get('hash')
 
     service_b_url = f"{SERVICE_DB_URL}/{h}"
-    logging.info(f'R: hash={h}, url={service_b_url}')
+    redirect_to = SERVICE_GUI_URL
 
+    logging.info(f'R: hash={h}, url={service_b_url}')
     response = requests.get(service_b_url)
 
-    if response.status_code != 200:
-        logging.info(f"R: no response")
-        return None
-    logging.info(f"R: response={response.text}")
-    return func.HttpResponse(status_code=302, headers={'Location': response.text})
+    if response.status_code == 200:
+        redirect_to = response.text
+    
+    logging.info(f"R: redirect to={redirect_to}")
+    return func.HttpResponse(status_code=302, headers={'Location': redirect_to})
